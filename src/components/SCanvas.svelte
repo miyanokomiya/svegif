fec3b5da4c190557fff98d878a4d1a81b42a19aa
miyanokomiya <tomoya.hstexze24632@gmail.com>
2020-08-02 17:12:44 +0900

    change zoom

--- src/components/SCanvas.svelte
+++ src/components/SCanvas.svelte
@@ -6,12 +6,32 @@
   export let height = 100;
   let scale = 1;
   let base = { x: 0, y: 0 };
-  let dragType: "" | "scroll" = "";
+  let dragType: "" | "scroll" | "zoom" = "";
+  let scaleSlider: HTMLElement | null;
+
+  const SCALE_BETA = 1.1;
+  const SCALE_RANGE = 20;
+  const SCALE_MAX = Math.pow(1.1, SCALE_RANGE / 2);
+  const SCALE_MIN = Math.pow(1.1, -SCALE_RANGE / 2);
+  $: scaleRate = Math.log(scale) / Math.log(SCALE_BETA);
+  $: scaleAnchorTop = `${(scaleRate / SCALE_RANGE + 1 / 2) * 100}%`;
+
+  function recalcZoom(pageY: number) {
+    const rect = scaleSlider.getBoundingClientRect();
+    const rate = (pageY - rect.top) / rect.height;
+    scale = Math.max(
+      Math.min(Math.pow(SCALE_BETA, SCALE_RANGE * (rate - 0.5)), SCALE_MAX),
+      SCALE_MIN
+    );
+  }
 
   const canvasDrag = useDrag((arg) => {
     switch (dragType) {
       case "scroll":
-        base = { x: base.x - arg.d.x, y: base.y - arg.d.y };
+        base = { x: base.x - arg.d.x * scale, y: base.y - arg.d.y * scale };
+        break;
+      case "zoom":
+        recalcZoom(arg.p.y);
         break;
     }
   });
@@ -19,36 +39,83 @@
     canvasDrag.onDown(e);
     dragType = "scroll";
   };
-  const onUp = (e: MouseEvent) => {
+  const onDownZoom = (e: MouseEvent) => {
+    canvasDrag.onDown(e);
+    dragType = "zoom";
+    recalcZoom(e.pageY);
+  };
+  const onUp = () => {
     dragType = "";
     canvasDrag.onUp();
   };
 </script>
 
-<svg
-  xmlns="http://www.w3.org/2000/svg"
-  preserveAspectRatio="none"
-  font-family="sans-serif"
-  viewBox="{`${base.x} ${base.y} ${width * scale} ${height * scale}`}"
-  {width}
-  {height}
+<div
+  class="root"
   on:mousedown|preventDefault="{onDownCanvas}"
   on:mousemove|preventDefault="{canvasDrag.onMove}"
   on:mouseup|preventDefault="{onUp}"
   on:mouseleave|preventDefault="{onUp}"
 >
-  {#if $currentScene.image}
-    <image
-      x="imageRect.x"
-      y="imageRect.y"
-      width="{$currentScene.image.width}"
-      height="{$currentScene.image.height}"
-      xlink:href="{$currentScene.image.base64}"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-    ></image>
-  {/if}
-</svg>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    preserveAspectRatio="none"
+    font-family="sans-serif"
+    viewBox="{`${base.x} ${base.y} ${width * scale} ${height * scale}`}"
+    {width}
+    {height}
+  >
+    {#if $currentScene.image}
+      <image
+        x="imageRect.x"
+        y="imageRect.y"
+        width="{$currentScene.image.width}"
+        height="{$currentScene.image.height}"
+        xlink:href="{$currentScene.image.base64}"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+      ></image>
+    {/if}
+  </svg>
+  <div bind:this="{scaleSlider}" class="scale-slider">
+    <div
+      class="bar"
+      on:mousedown|preventDefault|stopPropagation="{onDownZoom}"
+    ></div>
+    <div
+      class="anchor"
+      style="{`top: ${scaleAnchorTop}`}"
+      on:mousedown|preventDefault|stopPropagation="{onDownZoom}"
+    ></div>
+  </div>
+</div>
 
 <style lang="scss">
+  .root {
+    position: relative;
+  }
+  .scale-slider {
+    position: absolute;
+    bottom: 20px;
+    right: 20px;
+    height: 50%;
+    text-align: center;
 
+    .bar {
+      position: absolute;
+      transform: translateX(-50%);
+      width: 8px;
+      height: 100%;
+      background-color: #fff;
+      border-radius: 12px;
+    }
+    .anchor {
+      position: absolute;
+      transform: translate(-50%, -50%);
+      width: 20px;
+      height: 20px;
+      background-color: #888;
+      border-radius: 50%;
+      cursor: pointer;
+    }
+  }
 </style>
